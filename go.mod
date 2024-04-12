ef5f50ab692ec1ab74cf3fc60e705a4e9a5c106c
avGenie <aleksandr-generalov19@yandex.ru>
2024-04-12 09:15:16 +0300

    added logger middleware

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/avGenie/go-loyalty-system
+
+go 1.22.2
+
+require (
+	github.com/caarlos0/env/v10 v10.0.0
+	github.com/google/uuid v1.6.0
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
---
+++ internal/app/controller/http/middleware/logger/logger.go
@@ -0,0 +1,37 @@
+package http
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func LoggerMiddleware(h http.Handler) http.Handler {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		respData := &responseData{
+			statusCode: 0,
+			size:       0,
+		}
+		writer := logResponseWriter{
+			ResponseWriter: w,
+			responseData:   respData,
+		}
+		h.ServeHTTP(&writer, r)
+
+		duration := time.Since(start)
+
+		zap.L().Info(
+			"got incoming HTTP request",
+			zap.String("uri", r.RequestURI),
+			zap.String("method", r.Method),
+			zap.Duration("duration", duration),
+			zap.Int("status", respData.statusCode),
+			zap.Int("size", respData.size),
+		)
+	}
+
+	return http.HandlerFunc(logFn)
+}
